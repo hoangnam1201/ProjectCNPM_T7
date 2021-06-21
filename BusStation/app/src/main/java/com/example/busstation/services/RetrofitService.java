2f43a4606a12df890c817ff58dd3458b6f0cfc59
lamtvu <lamtvu1903@gmail.com>
2021-06-21 09:41:37 +0700

    update host;

--- BusStation/app/src/main/java/com/example/busstation/services/RetrofitService.java
+++ BusStation/app/src/main/java/com/example/busstation/services/RetrofitService.java
@@ -6,8 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitService {
     private static Retrofit Builder() {
         return new Retrofit.Builder()
-//                .baseUrl("https://busapbe.herokuapp.com/api/")
-                .baseUrl("http://192.168.1.7:3002/api/")
+                .baseUrl("https://busapbe.herokuapp.com/api/")
+//                .baseUrl("http://192.168.1.7:3002/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
